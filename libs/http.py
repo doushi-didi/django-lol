3dce0d52f4221ed647286148a301c14460343582
sunstar20133300 <m13917144383@163.com>
2019-03-19 21:06:41 +0800

    deal libs.http

---
+++ libs/http.py
@@ -0,0 +1,18 @@
+import json
+from django.conf import settings
+from django.http import HttpResponse
+from common.errors import OK
+
+def rend_json(data=None, code=OK):
+    '''将结果渲染成一个 Json数据的Httpresponse'''
+    result = {
+        'code': code,
+        'data': data,
+    }
+
+    if settings.DEBUG:
+        json_result = json.dumps(result, ensure_ascii=False, indent=4, sort_keys=True)
+    else:
+        json_result = json.dumps(result, ensure_ascii=False, separators=(',', ':'))
+
+    return HttpResponse(json_result)
